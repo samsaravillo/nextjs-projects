08440c69e1a1959c57ea3adfc53befc52e7a9d3d
Sam <sammys@fisgard.com>
2024-08-12 17:06:32 -0700

    added newpost component

--- todo-project/src/components/PostsList.jsx
+++ todo-project/src/components/PostsList.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
   return (
-    <ul className={classes.posts}>
-      <Post author="testko" body="testing body"></Post>
-      <Post author="testko" body="testing 234body"></Post>
-      <Post author="testko" body="testing b123ody"></Post>
-      <Post author="testko" body="testing bo5656dy"></Post>
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="testko" body={enteredBody}></Post>
+        <Post author="testko" body="testing 234body"></Post>
+        <Post author="testko" body="testing b123ody"></Post>
+        <Post author="testko" body="testing bo5656dy"></Post>
+      </ul>
+    </>
   );
 }
 
