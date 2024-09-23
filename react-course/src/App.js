cee890ef4330bad5f0431eb225f5b1438f12ef59
Sam <samsaravillo@gmail.com>
2024-09-23 10:50:34 -0700

    added react router dom

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,11 +1,24 @@
+import { Route } from "react-router-dom";
 import Todo from "./components/Todo";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
   return (
     <div>
+      {/* <Route path='/'>
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route> */}
       <h1>My Todos</h1>
       <Todo text='Learn react' />
-
     </div>
   );
 }
