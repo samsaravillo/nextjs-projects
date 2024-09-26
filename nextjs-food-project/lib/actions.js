93ce404fd9e66652358cce5119c90f27333d7cca
Sam <samsaravillo@gmail.com>
2024-09-26 00:15:16 -0700

    added validation in form and saveMeal service

--- nextjs-food-project/lib/actions.js
+++ nextjs-food-project/lib/actions.js
@@ -2,8 +2,13 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
+
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -12,6 +17,23 @@ export async function shareMeal(formData) {
     creator: formData.get("name"),
     creator_email: formData.get("email"),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: "Invalid Input.",
+    };
+  }
+
   await saveMeal(meal);
+  revalidatePath("/meals"); //this is to clear the cache E.g to target dynamic component revalidatePath("/meals", "layout") or ("/", "layout")
   redirect("/meals");
 }
