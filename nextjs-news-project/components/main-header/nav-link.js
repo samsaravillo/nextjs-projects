f8fce2d0ae069097ab65be4e242b8c446ed5ea57
Sam <samsaravillo@gmail.com>
2024-09-26 16:18:11 -0700

    intercepting routes and added modal to image

--- nextjs-news-project/components/main-header/nav-link.js
+++ nextjs-news-project/components/main-header/nav-link.js
@@ -2,19 +2,11 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import classes from "./nav-link.module.css";
 
 export default function NavLink({ href, children }) {
   const path = usePathname();
   return (
-    <Link
-      href={href}
-      className={
-        path.startsWith(href)
-          ? `${classes.link} ${classes.active}`
-          : classes.link
-      }
-    >
+    <Link href={href} className={path.startsWith(href) ? "active" : undefined}>
       {children}
     </Link>
   );
