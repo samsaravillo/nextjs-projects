fcfc14f77b73fff9661c99cdab72fcefbcf9e7c7
Sam <samsaravillo@gmail.com>
2024-09-25 22:54:23 -0700

    added new components and styles image picker and not found

--- nextjs-food-project/lib/meals.js
+++ nextjs-food-project/lib/meals.js
@@ -3,9 +3,12 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-  await new Promise((resolve) =>
-    setTimeout(resolve, 5000)
-  ); /*This is just for testing to delay the server calling this getMeals*/
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  /*This is just for testing to delay the server calling this getMeals*/
   //   throw new Error("Loading meals failed");
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
