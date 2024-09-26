674aabf71fbfe7a5479a75151e3c58e0e48d3097
Sam <samsaravillo@gmail.com>
2024-09-26 12:57:25 -0700

    added metadata to dynamic app router and components

--- nextjs-food-project/app/meals/[mealSlug]/page.js
+++ nextjs-food-project/app/meals/[mealSlug]/page.js
@@ -3,6 +3,19 @@ import { notFound } from "next/navigation";
 import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
 
+export async function generateMetaData({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
