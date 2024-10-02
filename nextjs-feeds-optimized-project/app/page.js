36e21ec6ec4fc5151b878769d6e7668ca8a72148
Sam <samsaravillo@gmail.com>
2024-10-01 22:18:22 -0700

    added dynamic metadata and optimized image

--- nextjs-feeds-optimized-project/app/page.js
+++ nextjs-feeds-optimized-project/app/page.js
@@ -1,7 +1,23 @@
-import { Suspense } from 'react';
+import { Suspense } from "react";
 
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+//static metadata
+// export const metadata = {
+//   title: "Latest Post",
+//   description: "Browse our latest post",
+// };
+
+//dynamic metadata
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all our ${numberOfPosts} posts.`,
+    description: "Browse all our posts.",
+  };
+}
 
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
@@ -13,10 +29,10 @@ export default async function Home() {
     <>
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
-      <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+      <section id='latest-posts'>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
   );
